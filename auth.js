df1657f9342fa91bb605575430566d533f7bab5d
darknil <jrmlelantus@gmail.com>
2023-04-07 21:24:29 +0200

    registration in main and functions in auth

--- auth.js
+++ auth.js
@@ -6,8 +6,8 @@ async function connectToDatabase() {
     const client = new MongoClient(uri);
     try {
       await client.connect();
-      console.log('Connected to database!');
       return client;
+
     } catch (err) {
       console.log(err);
     }
@@ -19,14 +19,16 @@ async function checkUserInDatabase(userId){
   try {
     const db = client.db('usersDB');
     const collection = db.collection('users');
+
     const query = { chatId: userId };
+    
     const result = await collection.findOne(query);
       if(result){
-        console.log(result);
         return true;
       }else{
         return false;
       }
+
   } catch (error) {
     console.error(err);
   }finally{
@@ -38,12 +40,20 @@ async function checkUserInDatabase(userId){
 async function addUserToDatabase(chatId, orgId, apiKey) {
   const client = await connectToDatabase();
   try {
-    
-    const db = client.db('usersDB');
-    const collection = db.collection('users');
-    const user = { chatId, orgId, apiKey, messages:[] };
-    await collection.insertOne(user);
-    console.log(`User with chat ID ${chatId} added to database!`);
+    const indb = await checkUserInDatabase(chatId);
+    if(!indb){
+      const db = client.db('usersDB');
+      const collection = db.collection('users');
+      
+      const user = { chatId, orgId, apiKey, messages:[] };
+      await collection.insertOne(user);
+  
+      console.log(`User with chat ID ${chatId} added to database!`);
+    }else{
+      return console.log('user with that id exist in db');
+    }
+
+
   } catch (error) {
     console.error(err);
   }finally{
@@ -52,6 +62,70 @@ async function addUserToDatabase(chatId, orgId, apiKey) {
 
     
   }
+// function for add orgid for user
+async function addOrgIdToUser(chatId, orgId) {
+  const client = await connectToDatabase();
+  try {
+    const db = client.db('usersDB');
+    const collection = db.collection('users');
+  
+    const filter = { chatId: chatId };
+    const update = { $set: { orgId: orgId } };
+  
+    const result = await collection.updateOne(filter, update);
+  
+    console.log(`${result.matchedCount} document(s) matched the filter criteria.`);
+    console.log(`${result.modifiedCount} document(s) was/were updated.`);
+  
+  } catch (error) {
+    console.error(err);
+  }finally{
+    client.close();
+  }
+}
+// function for add apikey for user
+async function addApiKeyToUser(chatId,apiKey){
+  const client = await connectToDatabase();
+  try {
+    const db = client.db('usersDB');
+    const collection = db.collection('users');
+
+    const filter = { chatId: chatId };
+    const update = { $set: { apiKey: apiKey } };
+
+    const result = await collection.updateOne(filter, update);
+    
+    console.log(`${result.matchedCount} document(s) matched the filter criteria.`);
+    console.log(`${result.modifiedCount} document(s) was/were updated.`);
+
+  } catch (error) {
+    console.error(err);
+  }finally{
+    client.close();
+  }
+}
+
+// function for get user field value
+async function getUserField(chatId, field){
+  // field can be 'chatId', 'orgId', 'apiKey'
+  const client = await connectToDatabase();
+  try {
+    const db = client.db('usersDB');
+    const collection = db.collection('users');
+
+    const filter = { chatId: chatId };
+    const options = { projection: { [field]: 1, _id: 0 } };
+
+    const result = await collection.findOne(filter, options);
+
+    return result ? result[field] : null;
+
+  } catch (error) {
+    console.error(err);
+  }finally{
+    client.close();
+  }
+}
 // function for update user messages array
 
 // function for getting user messages array
@@ -61,5 +135,8 @@ async function addUserToDatabase(chatId, orgId, apiKey) {
 
 module.exports ={
     addUserToDatabase,
-    checkUserInDatabase
+    checkUserInDatabase,
+    addOrgIdToUser,
+    addApiKeyToUser,
+    getUserField
 }
--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const { addUserToDatabase } = require('./auth');
 const { checkUserInDatabase } = require('./auth');
+const { addOrgIdToUser } = require('./auth');
+const { addApiKeyToUser } = require('./auth');
+const { getUserField } = require('./auth');
 ////////// Telegram variables
 const TelegramBot = require('node-telegram-bot-api');
 const token =process.env.TELEGRAM_TOKEN;
@@ -22,23 +25,70 @@ function loginfo(){
 const DONATE_TEXT = "crypto eth for everyone 0x7F23e289A9a8120Ea1bB2cCC71d388a415B6E4A5 \n\n Qiwi for RUB - qiwi.com/p/79609614863 \n\n PrivatBank for UAH - 4731185610368521 \n\n for USD - 4731185610882851 \n\n nfor EUR - 4731185614196258"
 //// Global vars
 //// Добавить хеширование паролей
+/// Добавить регистрацию
+/// Добавить обработку сообщений
 bot.setMyCommands([
-    {command: '/start', description: 'start bot'},
-    {command: '/donate', description: 'Support our project'},
-    {command: '/register', description: 'Support our project'}
+    {command: '/start', description: 'start'},
+    {command: '/register', description: 'register'},
+    {command: '/donate', description: 'donate'},
 ]);
 
-bot.onText(/\/start/,(msg)=>{
+bot.onText(/\/start/,async (msg)=>{
     const chatId = msg.chat.id;
     const messageforuser = process.env.START_TEXT;
     bot.sendMessage(chatId,messageforuser);
+    await addUserToDatabase(chatId);
 });
-bot.onText(/\/donate/,(msg)=>{
+bot.onText(/\/donate/,async (msg)=>{
     const chatId = msg.chat.id;
     bot.sendMessage(chatId,DONATE_TEXT );
 });
-bot.onText(/\/register/,(msg)=>{
+bot.onText(/\/register/,async (msg)=>{
   const chatId = msg.chat.id;
   const messageforuser = process.env.NOT_FOUND;
-  bot.sendMessage(chatId,messageforuser );
+  bot.sendMessage(chatId,messageforuser, {parse_mode: "Markdown"});
 });
+bot.onText(/\/orgid (.+)/,async (msg,match)=>{
+    const chatId = msg.chat.id;
+    const orgId = match[1];
+
+    await addOrgIdToUser(chatId,orgId);
+
+    const userOrgId = await getUserField(chatId,'orgId');
+    const userApikey = await getUserField(chatId,'apiKey');
+    const answer ='your orgId and Apikey:\n' + userOrgId + '\n' + userApikey;
+    
+    bot.sendMessage(chatId,answer);
+});
+bot.onText(/\/apikey (.+)/,async (msg,match)=>{
+    const chatId = msg.chat.id;
+    const apiKey = match[1];
+
+    await addApiKeyToUser(chatId,apiKey);
+
+    const userOrgId = await getUserField(chatId,'orgId');
+    const userApikey = await getUserField(chatId,'apiKey');
+    const answer ='your orgId and Apikey:\n' + userOrgId + '\n' + userApikey;
+
+    bot.sendMessage(chatId,answer)
+});
+//// /orgID "org_ID", /apikey "API_KEY"
+
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
+    
+    // Проверяем, начинается ли сообщение с "/"
+    if (text.startsWith('/')) {
+      // Если да, игнорируем сообщение
+      return;
+    }
+    const indb = await checkUserInDatabase(chatId);
+    if(indb){
+        bot.sendMessage(chatId,'one');
+    }else{
+        bot.sendMessage(chatId,"Your ID is not found in the database. Use /start");    
+    }
+    // Если нет, продолжаем обработку сообщения
+    // ...
+  });
